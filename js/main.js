eb1fe85b6b81f6f52e5d3520f9bb596e8fc981f2
Nguyên <nguyenkhanhngoctran@gmail.com>
2022-04-25 12:20:04 +0700

    Setup api folder
    - Add axios library
    - Add and setup axiosClient file
    - Add and setup postApi file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+import axiosClient from './api/axiosClient'
+import postApi from './api/postAPI'
+
 console.log('Hello')
+
+async function main() {
+  //   const response = await axiosClient.get('/posts')
+  const queryParams = {
+    _page: 2,
+    _lmit: '',
+  }
+  const response = await postApi.getAll(queryParams)
+  console.log(response)
+}
+main()
